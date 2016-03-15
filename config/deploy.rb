388640af431cad561436ba0f8eeea03cbe936d89
Gian Carlo Pace <giancarlo.pace@gmail.com>
2016-03-15 21:25:12 +0100

    new ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ role :db,  "giankavh"
 #$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 
 
-set :rvm_ruby_string, 'ruby-1.9.2-p290'        # Or whatever env you want it to run in.
+set :rvm_ruby_string, 'ruby-head'        # Or whatever env you want it to run in.
 #set :rvm_type, :system  # Copy the exact line. I really mean :user here
 #set :rvm_type, :user  # Copy the exact line. I really mean :user here
 
